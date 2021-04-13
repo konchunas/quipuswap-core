57e956ffe40ce1aafec66dceeefcaa07c311c7b2
Julian Konchunas <conscio@i.ua>
2021-04-13 14:41:34 +0300

    Add divest  after swaps tests

--- pytests/test_quick.py
+++ pytests/test_quick.py
@@ -74,8 +74,6 @@ class DexTest(TestCase):
         print(res.operations)
         (_, secondProfit) = parse_tez_transfer(res)
 
-        # print("balance expr", self.dex.context.get_amount_expr())
-
         # TODO it is actually super close to 12
         self.assertEqual(firstProfit+secondProfit, 11)
 
@@ -83,24 +81,39 @@ class DexTest(TestCase):
 
     def test_divest_everything(self):
         chain = LocalChain()
-        res = chain.execute(self.dex.initializeExchange(100000), amount=100000)
+        res = chain.execute(self.dex.initializeExchange(100_000), amount=100_000)
 
-        res = chain.execute(self.dex.divestLiquidity(min_tez=100000, min_tokens=100000, shares=100000), amount=0)
+        res = chain.execute(self.dex.divestLiquidity(min_tez=100_000, min_tokens=100_000, shares=100_000), amount=0)
 
-        print(res.operations)
-
-    def test_divest_everything(self):
-        chain = LocalChain()
-        res = chain.execute(self.dex.initializeExchange(100000), amount=100000)
-
-        res = chain.execute(self.dex.divestLiquidity(min_tez=100000, min_tokens=100000, shares=100000), amount=0)
-
-        print(res.operations)
         ops = parse_ops(res)
-        print(ops)
-        # param = parse_token_transfer(res)
-        # print(param)
 
-        # self.assertEqual(
+        self.assertEqual(ops[0]["type"], "token")
+        self.assertEqual(ops[0]["amount"], 100_000)
 
-    
+        self.assertEqual(ops[1]["type"], "tez")
+        self.assertEqual(ops[1]["amount"], 100_000)
+
+    def test_divest_amount_after_swap(self):
+        chain = LocalChain()
+        res = chain.execute(self.dex.initializeExchange(100_000), amount=100_000)
+
+        # swap tokens to tezos
+        res = chain.execute(self.dex.tokenToTezPayment(amount=10_000, min_out=1, receiver="tz1MDhGTfMQjtMYFXeasKzRWzkQKPtXEkSEw"), amount=0)
+        
+        ops = parse_ops(res)
+        tez_received = ops[1]["amount"]
+
+        # swap the received tezos back to tokens
+        res = chain.execute(self.dex.tezToTokenPayment(min_out=1, receiver="tz1MDhGTfMQjtMYFXeasKzRWzkQKPtXEkSEw"), amount=tez_received)
+
+        # take all the funds out
+        res = chain.execute(self.dex.divestLiquidity(min_tez=100_000, min_tokens=100_000, shares=100_000), amount=0)
+
+        ops = parse_ops(res)
+
+        self.assertEqual(ops[0]["type"], "token")
+        self.assertGreater(ops[0]["amount"], 100_000) # ensure greates cause it should include some fee
+
+        self.assertEqual(ops[1]["type"], "tez")
+        self.assertGreaterEqual(ops[1]["amount"], 100_000)
+
